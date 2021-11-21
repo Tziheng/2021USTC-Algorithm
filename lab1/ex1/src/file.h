4200338c1d5657ef506c864412e4255fd1877482
ziheng <67832547+Tziheng@users.noreply.github.com>
2021-11-21 13:07:08 +0800

    Delete lab1/ex1 directory

--- lab1/ex1/src/file.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FILE_H__
-#define __FILE_H__
-
-#define FILENAME_INPUT_DATA "..\\input\\1_1_input.txt"
-#define FILENAME_RESULT  "..\\output\\result.txt"
-#define FILENAME_TIME "..\\output\\time.txt"
-#define FILENAME_TABLE_M "..\\output\\table_m.csv"
-#define FILENAME_TABLR_S "..\\output\\table_s.csv"
-#endif
